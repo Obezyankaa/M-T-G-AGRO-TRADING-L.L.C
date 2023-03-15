768a1773769bef555bde3ba95bc0ed30f50171a2
VladAir <vladair@MacBook-Air-VladAir.local>
2023-03-15 12:01:25 +0300

    добавил поле контакты в скрытый блок

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,3 +172,15 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+let content = document.getElementById("content");
+let show = document.getElementById("showContent");
+let hide = document.getElementById("hideContent");
+
+show.addEventListener("click", () => {
+  content.style.display = "block";
+});
+
+hide.addEventListener("click", () => {
+  content.style.display = "none";
+});
