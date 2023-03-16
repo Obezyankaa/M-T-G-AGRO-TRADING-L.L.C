31ba5a606cd8ba9b72b838aa356ee38a08e70f7b
VladAir <vladair@MacBook-Air-VladAir.local>
2023-03-16 16:39:52 +0300

    немного изменил стили

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,15 +120,18 @@ const sr = ScrollReveal({
 })
 
 
-sr.reveal(`.home__data, .home__social-link, .home__info,
+sr.reveal(
+  `.home__data, .home__social-link, .home__info,
            .discover__container,
            .experience__data, .experience__overlay,
            .place__card,
-           .sponsor__content,
-           .footer__data, .footer__rights`,{
-    origin: 'top',
+           .sponsor__content, .sponsor__title,
+           .footer__data, .footer__rights`,
+  {
+    origin: "top",
     interval: 100,
-})
+  }
+);
 
 sr.reveal(`.about__data, 
            .video__description,
