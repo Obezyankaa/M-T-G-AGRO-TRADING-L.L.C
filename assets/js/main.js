ba38b6a97df96a6cd4c9d0b54666d48c91471675
VladAir <vladair@MacBook-Air-VladAir.local>
2023-04-05 07:50:56 +0300

    fix делаем открыкую первую карточку

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,6 +76,7 @@ function scrollUp() {
   // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
   if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
   else scrollUp.classList.remove("show-scroll");
+  console.log("функция  <-- scrollUp -->");
 }
 window.addEventListener("scroll", scrollUp);
 
